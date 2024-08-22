3017b02be4a42c95f4e2319ee8dd406a1d96c291
akosh001 <makhsudovakhrorbek@gmail.com>
2024-08-22 15:55:11 +0900

    fix: refactoring and B-TASK

--- train.js
+++ train.js
@@ -89,7 +89,7 @@ run();*/
 // Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
 // MASALAN countLetter("e", "engineer") 3ni return qiladi.
 
-function countLetters(letter, word) {
+/*function countLetters(letter, word) {
   let count = 0;
   for (let i = 0; i <= word.length; i++) {
     if (word[i] == letter) {
@@ -100,4 +100,19 @@ function countLetters(letter, word) {
 }
 
 const result = countLetters("a", "analytical");
+console.log("result:", result);*/
+
+// Masalani izohi B-TASK:
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+
+function countDigits(a) {
+  let count = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] >= 0) count++;
+  }
+  return count;
+}
+
+const result = countDigits("asd31234809gfd87676h56g");
 console.log("result:", result);
